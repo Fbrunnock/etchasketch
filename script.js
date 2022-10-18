ae15c11d82fa3cafe131b8125adb21a3bbe054cd
Fb <brunnockf@gmail.com>
2022-10-18 04:58:47 -0500

    create functional etch-a-sketch with input

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const cells = document.getElementsByClassName("cell");
-const resetButton = document.getElementById("resetButton")
+var cells = document.querySelectorAll('.cell')
+const resetButton = document.getElementById("resetButton");
+const sideCount = document.getElementById('sideNumber');
+const container = document.getElementById("container");
+var sideValue = document.getElementById('sidecount');
 
 for (let item of cells) {
-    item.addEventListener("mouseover", function(e){
-        item.classList.add('hoverColor')
+    item.addEventListener("mouseover", function(){
+        item.classList.add('hoverColor');
     })
 }
 
-resetButton.addEventListener("click", function(e){
+resetButton.addEventListener("click", function(){
     for (let item of cells){
-        item.classList.remove('hoverColor')
+        item.classList.remove('hoverColor');
     }
 });
 
+sideCount.addEventListener("mousedown",()=> {
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+})
+
+sideCount.addEventListener("mouseup",()=> {
+    for (let i = 0; i < (sideValue.value * sideValue.value); i++) {
+        let newCell = document.createElement('div');
+        newCell.className = "cell";
+        newCell.addEventListener("mouseover",()=>{
+            newCell.classList.add('hoverColor');
+        });
+        container.appendChild(newCell);
+    }
+    container.style.gridTemplateColumns = `repeat(${sideValue.value}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${sideValue.value}, 1fr)`
+})
